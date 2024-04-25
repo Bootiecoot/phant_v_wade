007dbc336d797a228c49190079630db4da198697
Bootiecoot <bootiecoot@gmail.com>
2024-04-25 11:31:55 -0700

    enemy

--- Plant_vs_Zombies_clone/Assets/Scripts/BasicEnemy.cs
+++ Plant_vs_Zombies_clone/Assets/Scripts/BasicEnemy.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class BasicEnemy : MonoBehaviour
 {
+
+    public float speed = 5.0f;
+    public Transform target;
+    private Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float step = speed * Time.deltaTime;
+
+        rb.velocity = -(Vector3.right * speed);
+
+
     }
 }
