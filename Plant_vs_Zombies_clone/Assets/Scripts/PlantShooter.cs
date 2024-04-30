8808aa9a49bea71a7bcdfc553709b6cc17ae6365
Bootiecoot <bootiecoot@gmail.com>
2024-04-30 11:36:11 -0700

    PlNT MAN

--- Plant_vs_Zombies_clone/Assets/Scripts/PlantShooter.cs
+++ Plant_vs_Zombies_clone/Assets/Scripts/PlantShooter.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlantShooter : MonoBehaviour
 {
+
+    public GameObject projectile;
+    public float firerate = 2.0;
+
     // Start is called before the first frame update
     void Start()
     {
