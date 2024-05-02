37ac1446646e8496954c38519d1e381ebd51be81
Corgi2013 <ryanmelin417@gmail.com>
2024-05-02 11:34:05 -0700

    UI

---
+++ Plant_vs_Zombies_clone/Assets/Scripts/UIController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+
+    public PlayerController playerController;
+    public TMP_Text sunText;
+    public TMP_Text costText;
+    public TMP_Text plantText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sunText.text = "Sun: " + playerController.sun.ToString();
+        costText.text = "Selected Plant Cost: " + playerController.selectedPlantCost.ToString();
+        plantText.text = "Selected Plant: " + playerController.selectedPlant.ToString();
+    }
+}
